8144f5425d7ae82cc2d4876efc0a2868a8771458
musamthembu84@gmail.com <Streetwise22@22>
2020-02-09 22:40:37 +0200

    project voteco on git

---
+++ src/views/Index.jsx
@@ -0,0 +1,343 @@
+
+import React from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "variables/charts.jsx";
+
+import Header from "components/Headers/Header.jsx";
+
+class Index extends React.Component {
+  state = {
+    activeNav: 1,
+    chartExample1Data: "data1"
+  };
+  toggleNavs = (e, index) => {
+    e.preventDefault();
+    this.setState({
+      activeNav: index,
+      chartExample1Data:
+        this.state.chartExample1Data === "data1" ? "data2" : "data1"
+    });
+    let wow = () => {
+      console.log(this.state);
+    };
+    wow.bind(this);
+    setTimeout(() => wow(), 1000);
+    // this.chartReference.update();
+  };
+  componentWillMount() {
+    if (window.Chart) {
+      parseOptions(Chart, chartOptions());
+    }
+  }
+  render() {
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+          <Row>
+
+            <Col className="mb-5 mb-xl-0" xl="8">
+              <Card className="bg-gradient-default shadow">
+
+              </Card>
+            </Col>
+            <Col className="order-xl-2 mb-5 mb-xl-0" xl="5">
+              <Card className="card-profile shadow">
+                <Row className="justify-content-center">
+                  <Col className="order-lg-2" lg="3">
+                    <div className="card-profile-image">
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="rounded-circle"
+                            src={require("assets/img/theme/eff.png")}
+                        />
+                      </a>
+                    </div>
+                  </Col>
+                </Row>
+                <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                  <div className="d-flex justify-content-between">
+                    <Button
+                        className="mr-4"
+                        color="info"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Connect
+                    </Button>
+                    <Button
+                        className="float-right"
+                        color="default"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Message
+                    </Button>
+                  </div>
+                </CardHeader>
+                <CardBody className="pt-0 pt-md-4">
+                  <Row>
+                    <div className="col">
+                      <div className="card-profile-stats d-flex justify-content-center mt-md-5">
+                        <div>
+                          <span className="heading">22</span>
+                          <span className="description">Friends</span>
+                        </div>
+                        <div>
+                          <span className="heading">10</span>
+                          <span className="description">Photos</span>
+                        </div>
+                        <div>
+                          <span className="heading">89</span>
+                          <span className="description">Comments</span>
+                        </div>
+                      </div>
+                    </div>
+                  </Row>
+                  <div className="text-center">
+                    <h3>
+                      Jessica Jones
+                      <span className="font-weight-light">, 27</span>
+                    </h3>
+                    <div className="h5 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Bucharest, Romania
+                    </div>
+                    <div className="h5 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      Solution Manager - Creative Tim Officer
+                    </div>
+                    <div>
+                      <i className="ni education_hat mr-2" />
+                      University of Computer Science
+                    </div>
+                    <hr className="my-4" />
+                    <p>
+                      Ryan — the name taken by Melbourne-raised, Brooklyn-based
+                      Nick Murphy — writes, performs and records all of his own
+                      music.
+                    </p>
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                      Show more
+                    </a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col className="mb-5 mb-xl-0" xl="5">
+              <Card className="card-profile shadow">
+                <Row className="justify-content-center">
+                  <Col className="order-lg-2" lg="3">
+                    <div className="card-profile-image">
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="rounded-circle"
+                            src={require("assets/img/theme/da.png")}
+                        />
+                      </a>
+                    </div>
+                  </Col>
+                </Row>
+                <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                  <div className="d-flex justify-content-between">
+                    <Button
+                        className="mr-4"
+                        color="info"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Connect
+                    </Button>
+                    <Button
+                        className="float-right"
+                        color="default"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Message
+                    </Button>
+                  </div>
+                </CardHeader>
+                <CardBody className="pt-0 pt-md-4">
+                  <Row>
+                    <div className="col">
+                      <div className="card-profile-stats d-flex justify-content-center mt-md-5">
+                        <div>
+                          <span className="heading">22</span>
+                          <span className="description">Friends</span>
+                        </div>
+                        <div>
+                          <span className="heading">10</span>
+                          <span className="description">Photos</span>
+                        </div>
+                        <div>
+                          <span className="heading">89</span>
+                          <span className="description">Comments</span>
+                        </div>
+                      </div>
+                    </div>
+                  </Row>
+                  <div className="text-center">
+                    <h3>
+                      Jessica Jones
+                      <span className="font-weight-light">, 27</span>
+                    </h3>
+                    <div className="h5 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Bucharest, Romania
+                    </div>
+                    <div className="h5 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      Solution Manager - Creative Tim Officer
+                    </div>
+                    <div>
+                      <i className="ni education_hat mr-2" />
+                      University of Computer Science
+                    </div>
+                    <hr className="my-4" />
+                    <p>
+                      Ryan — the name taken by Melbourne-raised, Brooklyn-based
+                      Nick Murphy — writes, performs and records all of his own
+                      music.
+                    </p>
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                      Show more
+                    </a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col className="mb-5 mb-xl-0" xl="5">
+              <Card className="card-profile shadow">
+                <Row className="justify-content-center">
+                  <Col className="order-lg-2" lg="3">
+                    <div className="card-profile-image">
+                      <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <img
+                            alt="..."
+                            className="rounded-circle"
+                            src={require("assets/img/theme/sasco.jpg")}
+                        />
+                      </a>
+                    </div>
+                  </Col>
+                </Row>
+                <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                  <div className="d-flex justify-content-between">
+                    <Button
+                        className="mr-4"
+                        color="info"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Connect
+                    </Button>
+                    <Button
+                        className="float-right"
+                        color="default"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                    >
+                      Message
+                    </Button>
+                  </div>
+                </CardHeader>
+                <CardBody className="pt-0 pt-md-4">
+                  <Row>
+                    <div className="col">
+                      <div className="card-profile-stats d-flex justify-content-center mt-md-5">
+                        <div>
+                          <span className="heading">22</span>
+                          <span className="description">Friends</span>
+                        </div>
+                        <div>
+                          <span className="heading">10</span>
+                          <span className="description">Photos</span>
+                        </div>
+                        <div>
+                          <span className="heading">89</span>
+                          <span className="description">Comments</span>
+                        </div>
+                      </div>
+                    </div>
+                  </Row>
+                  <div className="text-center">
+                    <h3>
+                      Jessica Jones
+                      <span className="font-weight-light">, 27</span>
+                    </h3>
+                    <div className="h5 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Bucharest, Romania
+                    </div>
+                    <div className="h5 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      Solution Manager - Creative Tim Officer
+                    </div>
+                    <div>
+                      <i className="ni education_hat mr-2" />
+                      University of Computer Science
+                    </div>
+                    <hr className="my-4" />
+                    <p>
+                      Ryan — the name taken by Melbourne-raised, Brooklyn-based
+                      Nick Murphy — writes, performs and records all of his own
+                      music.
+                    </p>
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                      Show more
+                    </a>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+
+          </Row>
+          <Row className="mt-5">
+
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Index;
---
+++ src/views/examples/Login.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col
+} from "reactstrap";
+
+class Login extends React.Component {
+
+  routeChange=()=> {
+    this.props.history.push("/admin/index");
+  }
+  render() {
+    return (
+      <>
+        <Col lg="5" md="7">
+          <Card className="bg-secondary shadow border-0">
+
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                <small>Use your credentials below</small>
+              </div>
+              <Form role="form">
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Login ID" type="text" />
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Password" type="password" />
+                  </InputGroup>
+                </FormGroup>
+                <div className="custom-control custom-control-alternative custom-checkbox">
+                  <input
+                    className="custom-control-input"
+                    id=" customCheckLogin"
+                    type="checkbox"
+                  />
+                  <label
+                    className="custom-control-label"
+                    htmlFor=" customCheckLogin"
+                  >
+                    <span className="text-muted">Remember me</span>
+                  </label>
+                </div>
+                <div className="text-center">
+                  <Button className="my-4" color="primary" type="button" onClick={this.routeChange}>
+                    Sign in
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </>
+    );
+  }
+}
+
+export default Login;
